e0831d767fcac9c92def95131c7abe8a0ce39e0d
ljs <ljs050@outlook.com>
2025-09-23 14:47:36 +0800

    20250923

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -99,3 +99,142 @@ def monte_carlo_mix(
         "items_properties": properties_distribution,
         "destroy_distribution": destroys,
     }
+
+Strategy = Callable[[Item], Scroll]
+StopCondition = Callable[[Item], bool]
+# ========== 策略工厂 ==========
+def strategy_fixed_sequence(
+    seq: List[str],
+    SCROLL_SET: Dict[str, Scroll]
+    ) -> Strategy:
+    """
+    固定序列：按照提供的卷轴名序列循环使用（长度不足时可循环或截断）。
+    例如：["C", "C", "B", "B", "B", "B", "B"]
+    """
+
+    scrolls = [SCROLL_SET[name] for name in seq]
+
+    def strat(state: Item) -> Scroll:
+        if state.attempts_used < state.num_slots:
+            idx = min(state.attempts_used, len(seq) - 1)
+            return scrolls[idx]
+        else:
+            raise ValueError("No more slots to update")
+    return strat
+
+# ========== 目标工厂 ==========
+def stop_all_success() -> StopCondition:
+    # 目标：在不爆装情况下，全成功
+    def cond(state: Item) -> bool:
+        return (np.sum(state.results_history) == state.num_slots) and (not state.destroyed)
+    return cond
+
+def stop_atk_at_least(target_atk: int) -> StopCondition:
+    # 目标：总攻击力达到阈值（不爆装）
+    def cond(state: Item) -> bool:
+        return (state.atk_value >= target_atk) and (not state.destroyed)
+    return cond
+
+# ========== 流程工厂 ==========
+def stop_when_fail_or_consume_all_slots(
+    strategy: Strategy,
+    stop_condition: StopCondition,
+    stop_if_fail: bool = False, # 是否失败就不再砸卷
+) -> list:
+
+    def run_procedure(rng: np.random.Generator,
+                    item_template: Item,
+    ):
+        all_items_list = []
+
+        while True:
+
+            item_state = copy.deepcopy(item_template)
+            # 对每件装备的砸卷处理
+            while item_state.attempts_used < item_state.num_slots:
+                scroll = strategy(item_state)
+
+                # 记录本次使用的卷轴
+                item_state.history.append(scroll.name)
+
+                # 消耗一次尝试
+                item_state.attempts_used += 1
+
+                # 判定
+                if rng.random() < scroll.success_p:
+                    item_state.atk_value += scroll.atk_value
+                    item_state.power_value += scroll.power_value
+                    item_state.ag_value += scroll.ag_value
+                    item_state.int_value += scroll.int_value
+                    item_state.lucky_value += scroll.lucky_value
+
+                    item_state.results_history.append(True)
+                else:
+                    item_state.results_history.append(False)
+
+                    # 判定该装备是否消失
+                    if scroll.destroy_on_fail_p > 0 and (rng.random() < scroll.destroy_on_fail_p):
+                        item_state.destroyed = True
+                        break
+                    
+                    # 新策略：只要失败，就不再继续砸
+                    if stop_if_fail:
+                        break
+
+            all_items_list.append(item_state)
+            # 完成该装备的一轮砸卷后
+            if stop_condition(item_state):
+                return all_items_list
+    
+    return run_procedure
+
+def stop_on_first_fail(
+    strategy: Strategy,
+    stop_condition: StopCondition,
+) -> list:
+
+    def run_procedure(rng: np.random.Generator,
+                    item_template: Item,
+    ):
+        all_items_list = []
+
+        while True:
+
+            item_state = copy.deepcopy(item_template)
+            # 对每件装备的砸卷处理
+            while item_state.attempts_used < item_state.num_slots:
+                scroll = strategy(item_state)
+
+                # 记录本次使用的卷轴
+                item_state.history.append(scroll.name)
+
+                # 消耗一次尝试
+                item_state.attempts_used += 1
+
+                # 判定
+                if rng.random() < scroll.success_p:
+                    item_state.atk_value += scroll.atk_value
+                    item_state.power_value += scroll.power_value
+                    item_state.ag_value += scroll.ag_value
+                    item_state.int_value += scroll.int_value
+                    item_state.lucky_value += scroll.lucky_value
+
+                    item_state.results_history.append(True)
+                else:
+                    item_state.results_history.append(False)
+
+                    # 判定该装备是否消失
+                    if scroll.destroy_on_fail_p > 0 and (rng.random() < scroll.destroy_on_fail_p):
+                        item_state.destroyed = True
+                        break
+                    
+                    # 第一次失败就不砸了
+                    if item_state.attempts_used == 1:
+                        break
+
+            all_items_list.append(item_state)
+            # 完成该装备的一轮砸卷后
+            if stop_condition(item_state):
+                return all_items_list
+    
+    return run_procedure
